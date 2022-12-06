eab50bb47ecca3bb5d8907b67b112e2e365e3bbb
Mihey-Melnikov <55349287+Mihey-Melnikov@users.noreply.github.com>
2022-12-06 11:52:59 +0500

    add task 3

---
+++ task3.cs
@@ -0,0 +1,67 @@
+﻿namespace ConsoleApp1;
+
+public class ConcurrentStack<T> : IStack<T>
+{
+    private Node? _headNode;
+
+    public void Push(T item)
+    {
+        var currentNode = new Node(item, _headNode, (_headNode?.Count ?? 0) + 1);
+        if (Interlocked.CompareExchange(ref _headNode, currentNode, currentNode.Next) == currentNode.Next)
+            return;
+        var spinWait = new SpinWait();
+        do
+        {
+            spinWait.SpinOnce(-1);
+            currentNode.Next = _headNode;
+        }
+        while (Interlocked.CompareExchange(ref _headNode, currentNode, currentNode.Next) != currentNode.Next);
+    }
+
+    public bool TryPop(out T? item)
+    {
+        var prevNode = _headNode;
+        if (prevNode == null)
+        {
+            item = default;
+            return false;
+        }
+        if (Interlocked.CompareExchange(ref _headNode, prevNode.Next, prevNode) == prevNode)
+        {
+            item = prevNode.Value;
+            return true;
+        }
+        var spinWait = new SpinWait();
+        while (true)
+        {
+            prevNode = _headNode;
+            if (prevNode == null)
+            {
+                item = default;
+                return false;
+            }
+            if (Interlocked.CompareExchange(ref _headNode, prevNode.Next, prevNode) == prevNode)
+            {
+                item = prevNode.Value;
+                return true;
+            }
+            spinWait.SpinOnce(-1);
+        }
+    }
+
+    public int Count => _headNode?.Count ?? 0;
+
+    private class Node
+    {
+        public T Value { get; }
+        public Node? Next { get; set; }
+        public int Count { get; }
+
+        public Node(T value, Node? next, int count)
+        {
+            Value = value;
+            Next = next;
+            Count = count;
+        }
+    }
+}
