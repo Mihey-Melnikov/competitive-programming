d777cff40ec22b5b7c4e87a777b88b1ad6b9c4b2
Mihey-Melnikov <55349287+Mihey-Melnikov@users.noreply.github.com>
2022-12-12 23:40:38 +0500

    Add files via upload

---
+++ SequentialLogParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DataParallelismTask.LogParsers
+{
+    public class SequentialLogParser : ILogParser 
+    {
+        private readonly FileInfo _file;
+        private readonly Func<string, string?> _tryGetIdFromLine;
+
+        public SequentialLogParser(FileInfo file, Func<string, string?> tryGetIdFromLine)
+        {
+            _file = file;
+            _tryGetIdFromLine = tryGetIdFromLine;
+        }
+        
+        public string[] GetRequestedIdsFromLogFile()
+        {
+            var lines = File.ReadLines(_file.FullName);
+            return lines
+                .Select(_tryGetIdFromLine)
+                .Where(id => id != null)
+                .ToArray()!;
+        }
+    }
+}
---
+++ ThreadLogParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace DataParallelismTask.LogParsers
+{
+    public class ThreadLogParser : ILogParser
+    {
+        private readonly FileInfo _file;
+        private readonly Func<string, string?> _tryGetIdFromLine;
+
+        public ThreadLogParser(FileInfo file, Func<string, string?> tryGetIdFromLine)
+        {
+            _file = file;
+            _tryGetIdFromLine = tryGetIdFromLine;
+        }
+
+        public string[] GetRequestedIdsFromLogFile()
+        {
+            var linesList = File.ReadLines(_file.FullName).ToList();
+            var threadsCount = Environment.ProcessorCount;
+            var partSize = linesList.Count / threadsCount;
+            var parts = new IEnumerable<string>[threadsCount];
+
+            var partsList = linesList
+                .Select((line, index) => new { Value = line, Index = index })
+                .Where(line => line.Index / partSize < threadsCount)
+                .GroupBy(line => line.Index / partSize)
+                .Select(lineGroup => lineGroup.Select(line => line.Value)).ToList();
+            
+            var threads = new List<Thread>();
+            for(var i = 0; i < linesList.Count % threadsCount; i++) 
+                partsList[i] = partsList[i].Append(linesList[linesList.Count - i - 1]);
+            
+            var currentPartIndex = 0;
+            foreach (var thread in from part in partsList let index = currentPartIndex select new Thread(() => Parsing(index, part, parts)))
+            {
+                threads.Add(thread);
+                currentPartIndex++;
+            }
+            
+            foreach (var thread in threads) thread.Start();
+            foreach (var thread in threads) thread.Join();
+            
+            return parts.SelectMany(part => part).ToArray();
+        }
+
+        private void Parsing(int index, IEnumerable<string> linesToParse, IList<IEnumerable<string>> parts)
+        {
+            parts[index] = linesToParse
+                .Select(_tryGetIdFromLine)
+                .Where(id => id != null)
+                .ToArray()!;
+        }
+    }
+}
